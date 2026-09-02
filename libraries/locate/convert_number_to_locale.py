9f9ffdb926d5d161fbb7334a2a8b00f983eb4572
snennis <ddennisddv@gmail.com>
2026-09-02 20:34:10 +0200

    import locale and init function

---
+++ libraries/locate/convert_number_to_locale.py
@@ -0,0 +1,9 @@
+"""This Function converts a given number (int OR float) into the
+   correct format of the current locale of the user."""
+
+# library imports
+import locale
+
+def convert_number_to_locale(number: int or float):
+
+    pass
