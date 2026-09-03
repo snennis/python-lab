e3c3c6a1b8790256a54a77e0ad04329c77f75763
snennis <ddennisddv@gmail.com>
2026-09-03 17:34:06 +0200

    delete .

--- libraries/locate/convert_number_to_locale.py
+++ libraries/locate/convert_number_to_locale.py
@@ -1,5 +1,5 @@
 """This Function converts a given number (int OR float) into the
-   correct format of the current locale of the user."""
+   correct format of the current locale of the user"""
 
 # library imports
 import locale
